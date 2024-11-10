bd56f4c564dcd1f2c1b646244f03af7c4c9d7dbe
DieMbaye <diem63977@gmail.com>
2024-11-10 20:03:03 +0000

    test

--- ConnectDB.php
+++ ConnectDB.php
@@ -9,7 +9,7 @@
     $db = "DA_Clinique";
     $conn = mysqli_connect($host, $user, $password, $db);
     if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        echo "Failed to connecte to MySQL: " . mysqli_connect_error();
         exit();
     }
     ?>
